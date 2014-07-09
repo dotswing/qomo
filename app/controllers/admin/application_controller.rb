a8b5830b16f814d6dcb16eb9f159d9babc71cd18
Xingjian  Xu <dotswing@gmail.com>
2014-07-09 13:46:25 +0800

    QOMO-9 Only admin users can view admin panel

--- app/controllers/admin/application_controller.rb
+++ app/controllers/admin/application_controller.rb
@@ -1,5 +1,16 @@
 class  Admin::ApplicationController < ApplicationController
   layout 'admin'
+
   before_action :authenticate_user!
+
+  before_action :require_admin
+
+  private
+
+  def require_admin
+    unless current_user.admin?
+      redirect_to status: 403
+    end
+  end
 
 end
