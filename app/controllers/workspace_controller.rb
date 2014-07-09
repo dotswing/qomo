d588b8292ef56b92e23f9e355cc03e1ff47209f5
Xingjian  Xu <dotswing@gmail.com>
2014-07-09 13:37:59 +0800

    QOMO-10 Dispaly correct content in left sidebar in workspace

--- app/controllers/workspace_controller.rb
+++ app/controllers/workspace_controller.rb
@@ -1,4 +1,7 @@
 class WorkspaceController < ApplicationController
+
   def index
+    @groups = ToolGroup.all
   end
+
 end
--- app/models/tool_group.rb
+++ app/models/tool_group.rb
@@ -1,3 +1,5 @@
 class ToolGroup < ActiveRecord::Base
+  default_scope { order('title DESC') }
+
   has_many :tools, foreign_key: 'group_id'
 end
