9668a31b6cfb1bd007aed4bdd9b6a5a6bddb380a
Xingjian  Xu <dotswing@gmail.com>
2014-10-22 13:15:14 +0800

    QOMO-97 #close Load a pipeline which is not owned by you

--- app/controllers/workspace_controller.rb
+++ app/controllers/workspace_controller.rb
@@ -110,8 +110,21 @@ class WorkspaceController < ApplicationController
 
 
   def load
+    pipeline = Pipeline.find params['id']
+    if pipeline.owner.id != current_user.id
+      my_pipeline = Pipeline.new title: 'My ' + pipeline.title,
+                                 desc: pipeline.desc,
+                                 boxes: pipeline.boxes,
+                                 connections: pipeline.connections,
+                                 params: pipeline.params,
+                                 owner_id: current_user.id
+
+      my_pipeline.save
+      pipeline = my_pipeline
+    end
+
     flash[:action] = 'load'
-    flash[:pid] = params['id']
+    flash[:pid] = pipeline.id
     redirect_to action: 'index'
   end
 
