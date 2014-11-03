e428badf57e73884a2dc0cb93c3c3ce2cd941e44
Xingjian  Xu <dotswing@gmail.com>
2014-11-03 14:18:28 +0800

    QOMO-118 #close Reference cite link for tool

--- app/helpers/tools_helper.rb
+++ app/helpers/tools_helper.rb
@@ -1,2 +1,12 @@
 module ToolsHelper
+
+  def tool_ref(tool)
+    <<REF
+  #{tool.contributor} (#{tool.created_at.strftime '%Y'}) Tool: "#{tool.title}" in Qomo paltform,
+  available at
+  <em>#{tool_url(tool)}</em>
+  (Last update: #{tool.updated_at.strftime '%b %d, %Y'}).
+REF
+  end
+
 end
