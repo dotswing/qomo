de318d5aadf4e9ed6d6488817b16bac07009063e
Xingjian  Xu <dotswing@gmail.com>
2014-11-06 09:57:14 +0800

    QOMO-136 #close Cannot get git version when deploy using DVM

--- config/application.rb
+++ config/application.rb
@@ -6,7 +6,13 @@ Bundler.require(*Rails.groups)
 
 module Qomo
   class Application < Rails::Application
-    GIT_VERSION = `git rev-parse --short HEAD`
+    revision_file = File.join(Rails.root, 'REVISION')
+    if File.exist? revision_file
+      GIT_VERSION = File.read revision_file
+    else
+      GIT_VERSION = `git rev-parse --short HEAD`
+    end
+
 
     config.time_zone = 'Beijing'
     config.autoload_paths << Rails.root.join('lib')
